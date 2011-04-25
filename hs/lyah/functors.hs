a57ce6d2361b3c6cd43b67f55875913c02d57add
Brian Brooks <brooks.brian@gmail.com>
2011-04-24 21:02:54 -0400

    more functors, newtype

--- hs/lyah/functors.hs
+++ hs/lyah/functors.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 
 {-functors are things that can be mapped over. described by Functor typeclass
   which only has one typeclass method fmap :: (a -> b) -> f a -> f b which
@@ -40,8 +41,8 @@
    takes a function from 'a' to 'b' and a function from 'r' to 'a' and returns
    a function from 'r' to 'b'. this is like function composition.-}
 
-instance Functor ((->) r) where
-  fmap = (.)
+-- instance Functor ((->) r) where
+--   fmap = (.)
 
 -- *Main> let a = fmap (*) [1,2,3,4]
 -- *Main> :t a
@@ -49,9 +50,9 @@ instance Functor ((->) r) where
 -- *Main> fmap (\f -> f 9) a
 -- [9,18,27,36]
 
-class (Functor f) => Applicative f where
-  pure :: a -> f a
-  (<*>) :: f (a -> b) -> f a -> f b
+-- class (Functor f) => Applicative f where
+--   pure :: a -> f a
+--   (<*>) :: f (a -> b) -> f a -> f b
 
 -- pure should take a value of any type and return an applicative functor
 -- with that value inside it. better way of thinking about pure is that
@@ -62,10 +63,10 @@ class (Functor f) => Applicative f where
 -- functor and extracts that function from the first functor and then
 -- maps it over the second one.
 
-instance Applicative Maybe where
-  pure = Just
-  Nothing <*> _ = Nothing
-  (Just f) <*> something = fmap f something
+-- instance Applicative Maybe where
+--   pure = Just
+--   Nothing <*> _ = Nothing
+--   (Just f) <*> something = fmap f something
 
 -- *Main> Just (+3) <*> Just 9
 -- Just 12
@@ -78,15 +79,15 @@ instance Applicative Maybe where
 -- instead of writing: pure f <*> x <*> y <*> ..., write: fmap f x <*> y <*> ...
 -- Control.Applicative exports a function <$> which is fmap as an infix op
 
-(<$>) :: (Functor f) => (a -> b) -> f a -> f b
-f <$> x = fmap f x
+-- (<$>) :: (Functor f) => (a -> b) -> f a -> f b
+-- f <$> x = fmap f x
 
 -- *Main> (++) <$> Just "john " <*> Just "travolta"
 -- Just "john travolta"
 
-instance Applicative [] where
-  pure x = [x]
-  fs <*> xs = [f x | f <- fs, x <- xs]
+-- instance Applicative [] where
+--   pure x = [x]
+--   fs <*> xs = [f x | f <- fs, x <- xs]
 
 -- *Main> pure "Hey" :: [String]
 -- ["Hey"]
@@ -110,12 +111,12 @@ instance Applicative [] where
 -- *Main> filter (>50) $ (*) <$> [2,5,10] <*> [8,10,11]
 -- [55,80,100,110]
 
-instance Applicative IO where
-  pure = return
-  a <*> b = do
-    f <- a
-    x <- b
-    return (f x)
+-- instance Applicative IO where
+--   pure = return
+--   a <*> b = do
+--     f <- a
+--     x <- b
+--     return (f x)
 
 myAction :: IO String
 -- myAction = do
@@ -131,7 +132,153 @@ myAction = (++) <$> getLine <*> getLine
 -- if you ever bind I/O actions to names then call some function on them and wrap
 -- the result in a return, consider applicative style like above
 
-instance Applicative ((->) r) where
-  pure x = (\_ -> x)
-  f <*> g = \x -> f x (g x)
+-- instance Applicative ((->) r) where
+--   pure x = (\_ -> x)
+--   f <*> g = \x -> f x (g x)
 
+-- *Main> (pure 3) "Blah"
+-- 3
+-- *Main> :t (+) <$> (+3) <*> (*100)
+-- (+) <$> (+3) <*> (*100) :: Num b => b -> b
+-- *Main> (+) <$> (+3) <*> (*100) $ 5
+-- 508
+-- *Main> (\x y z -> [x,y,z]) <$> (+3) <*> (*2) <*> (/2) $ 5
+-- [8.0,10.0,2.5]
+
+-- instance Applicative ZipList where
+--   pure x = ZipList (repeat x)
+--   ZipList fs <*> ZipList xs = ZipList (zipWith (\f x -> f x) fs xs)
+
+-- <*> applies the first function to the first value, the second function
+-- to the second value, etc..
+
+-- pure "haha" equals ZipList (["haha","haha",...
+
+-- *Main> :t getZipList
+-- getZipList :: ZipList a -> [a]
+-- *Main> getZipList $ (+) <$> ZipList [1,2,3] <*> ZipList [100,100,100]
+-- [101,102,103]
+-- *Main> getZipList $ (,,) <$> ZipList "dog" <*> ZipList "cat" <*> ZipList "bird"
+-- [('d','c','b'),('o','a','i'),('g','t','r')]
+-- (,,) equals \x y z -> (x,y,z)
+
+-- *Main> :t liftA2
+-- liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+-- liftA2 f a b = f <$> a <*> b
+
+-- *Main> fmap (\x -> [x]) (Just 4)
+-- Just [4]
+
+-- *Main> liftA2 (:) (Just 3) (Just [4])
+-- Just [3,4]
+-- *Main> (:) <$> Just 3 <*> Just [4]
+-- Just [3,4]
+
+sequenceA :: (Applicative f) => [f a] -> f [a]
+-- sequenceA [] = pure []
+-- sequenceA (x:xs) = (:) <$> x <*> sequenceA xs
+sequenceA = foldr (liftA2 (:)) (pure [])
+
+-- *Main> sequenceA [Just 3, Just 2, Just 1]
+-- Just [3,2,1]
+-- *Main> sequenceA [Just 3, Nothing, Just 1]
+-- Nothing
+-- *Main> sequenceA [(+3),(+2),(+1)] 3
+-- [6,5,4]
+-- *Main> sequenceA [[1,2,3],[4,5,6]]
+-- [[1,4],[1,5],[1,6],[2,4],[2,5],[2,6],[3,4],[3,5],[3,6]]
+-- *Main> sequenceA [[1,2,3],[4,5,6],[]]
+-- []
+
+-- does number satisfy all predicates in list?
+
+-- *Main> map (\f -> f 7) [(>4),(<10),odd]
+-- [True,True,True]
+-- *Main> and $ map (\f -> f 7) [(>4),(<10),odd]
+-- True
+-- *Main> and $ sequenceA [(>4),(<10),odd] 7
+-- True
+
+{- Ways of list type being an applicative functor:
+   <*> - take functions out of list to left, apply to
+         every element of list to right. every possible
+         combination of applying functions to elemnts
+   ZipList - take 1st function from list to left apply to
+             1st arg in list to right, 2nd ..., all the way down.
+   
+*Main> [(+1),(*100),(*5)] <*> [1,2,3]
+[2,3,4,100,200,300,5,10,15]
+*Main> getZipList $ ZipList [(+1),(*100),(*5)] <*> ZipList [1,2,3]
+[2,200,15]
+
+data ZipList a = ZipList [a]
+data ZipList a = ZipList { getZipLists :: [a] }
+
+use newtype when you want to take a type and wrap it in something else.
+e.g. ZipList a == [a]
+
+newtype ZipList a = ZipList { getZipList :: [a] }
+
+newtype is faster. 'data' boxes things...
+
+when you use newtype, can only have one value constructor with one field.
+with data you can have several value constructors with zero or more fields. -}
+
+newtype CharList = CharList { getCharList :: [Char] } deriving (Eq,Show)
+-- *Main> :t CharList
+-- CharList :: [Char] -> CharList
+
+{-
+
+class Functor f where
+  fmap :: (a -> b) -> f a -> f b
+
+instance Functor Maybe where
+   fmap ...
+
+implement fmap, looks something like
+fmap :: (a -> b) -> Mayber a -> Maybe b
+
+what if we want to make tuple an instance of Functor such that fmap
+only applys f to the first component of tuple? e.g.
+fmap (+3) (1,1) equals (4,1)
+writing that instance is kind of hard, so use newtype Pair :) -}
+
+newtype Pair b a = Pair { getPair :: (a,b) }
+instance Functor (Pair c) where
+  fmap f (Pair (x,y)) = Pair (f x, y)
+--fmap :: (a -> b) -> Pair c a -> Pair c b
+-- *Main> getPair $ fmap (*100) (Pair (2,3))
+-- (200,3)
+
+
+-- *Main> undefined
+-- *** Exception: Prelude.undefined
+-- *Main> head [3,4,undefined,6]
+-- 3
+
+data CoolBool = CoolBool { getCoolBool :: Bool }
+
+helloMe :: CoolBool -> String
+helloMe (CoolBool _) = "hello"
+
+-- *Main> helloMe undefined
+-- "*** Exception: Prelude.undefined
+
+-- data can have multiple value constructors, so to see if our function conforms to
+-- (CoolBool _) pattern, Haskell has to evaluate the value..
+
+newtype BetterBool = BetterBool { getBetterBool :: Bool }
+
+helloMe' :: BetterBool -> String
+helloMe' (BetterBool _) = "hello"
+
+-- *Main> helloMe' undefined
+-- "hello"
+
+-- LAZIER...
+
+
+{-
+
+  type keywork for making type synonyms

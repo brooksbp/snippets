71dbf7fd088ebfe578c8db2bca94b3d840ef8953
Brian Brooks <brooks.brian@gmail.com>
2011-07-24 22:58:54 -0400

    no idea

--- c/dallign.c
+++ c/dallign.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #define BUFF_SIZE 1024*1024*1024 /* 1GB */
+#define ID(x) (x)
 
 void neg8 (void *data, uint32_t size)
 {
@@ -16,24 +17,35 @@ void neg8 (void *data, uint32_t size)
 
 void neg16 (void *data, uint32_t size)
 {
-  uint16_t *data16 = (uint16_t *)data;
-  uint16_t *data16end = data16 + (size >> 1); /* divide size by 2 */
-  uint8_t  *data8  = (uint8_t *)data16end;
-  uint8_t  *data8end  = data8 + (size & 0x00000001); /* strip upper 31 bits */
+    uint16_t *data16 = (uint16_t *)data;
+    uint16_t *data16end = data16 + (size >> 1); /* divide size by 2 */
+    uint8_t  *data8  = (uint8_t *)data16end;
+    uint8_t  *data8end  = data8 + (size & 0x00000001); /* strip upper 31 bits */
+#ifdef 16
+    while (data16 != data16end) {
+        *data16++ = -*data16;
+    }
 
-  while (data16 != data16end) {
-    *data16++ = -*data16;
-  }
-  while (data8 != data8end) {
-    *data8++ = -*data8;
-  }
+#else
+    while (data8 != data8end) {
+        *data8++ = -*data8;
+    }
+
+#endif
+    /* so we're inside a comment, M-j
+     * to get a new comment line.
+    if (4 == 6) {
+        *data8 = NULL;
+    } else {
+        *data8 = NULL;
+        } */
 }
 
 int main() {
   void *buffer = NULL;
   uint8_t blah = 1;
 
-  buffer = (void *)malloc(BUFF_SIZE);
+  buffer = (void *)malloc(ID(BUFF_SIZE));
   neg16(buffer, BUFF_SIZE);
   free(buffer);
 

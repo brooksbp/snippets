855a7eac18391a792e648df9e36c1799a32c9442
Brian Brooks <brooks.brian@gmail.com>
2012-04-24 18:52:34 -0400

    * Setup playground for The Joy of Clojure. clojure-mode and Swank Clojure / SLIME working.. bingo!

---
+++ clj/tjoc/src/tjoc/core.clj
@@ -0,0 +1,2 @@
+(ns tjoc.core)
+
---
+++ clj/tjoc/test/tjoc/test/core.clj
@@ -0,0 +1,6 @@
+(ns tjoc.test.core
+  (:use [tjoc.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

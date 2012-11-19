253b657def3ac2c2d4b487120297f124c5e52be9
Brian Brooks <brooks.brian@gmail.com>
2012-11-19 18:04:42 -0500

    fix 001/main2.cc

--- project-euler/001/main2.cc
+++ project-euler/001/main2.cc
@@ -8,26 +8,14 @@ int main(void) {
   int acc = 0;
 
   int m3, m5;
-
-  int i = 1;
-  while (++i) {
-    m3 = i * 3;
+  for (int i = 0; i < N; i++) {
+    m3 = 3 * i;
+    m5 = 5 * i;
     if (m3 < N) {
       acc += m3;
-    } else {
-      break;
     }
-  }
-
-  i = 1;
-  while (i++) {
-    m5 = i * 5;
-    if (m5 < N) {
-      if ((m5 % 3) != 0) {
-        acc += m5;
-      }
-    } else {
-      break;
+    if (m5 < N && (m5 % 3) != 0) {
+      acc += m5;
     }
   }
   

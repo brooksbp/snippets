6f67864c780c66bbf62ec2047559f161782511d4
Brian Brooks <brooks.brian@gmail.com>
2013-01-03 12:44:12 -0500

    1.2.cc

--- cci/1.2.cc
+++ cci/1.2.cc
@@ -1,2 +1,37 @@
 // Implement a function void reverse(char* str) which reverses
 // a null-terminated string.
+
+#include <iostream>
+#include <cstring>
+#include <cassert>
+
+using namespace std;
+
+void reverse(char* str) {
+  char *a = str;
+  char *b = a + strlen(str) - 1;
+  int done;
+  if (strlen(str) < 2) return;
+  do {
+    char tmp = *a; *a = *b; *b = tmp;
+    done = a == b || a == (b - 1);
+    a += 1; b -= 1;
+  } while (!done);
+}
+
+int main(int argc, char *argv[]) {
+  char* ro_str = "123abc"; // RODATA string literal!!
+
+#define RUN_ASSERT(strf, strr) do {    \
+    string s(strf);                    \
+    reverse((char *) s.c_str());       \
+    assert(!s.compare(strr));          \
+  } while (0)
+  
+  RUN_ASSERT("", "");
+  RUN_ASSERT("a", "a");
+  RUN_ASSERT("ab", "ba");
+  RUN_ASSERT("123abc", "cba321");
+  RUN_ASSERT("123xabc", "cbax321");  
+  return 0;
+}

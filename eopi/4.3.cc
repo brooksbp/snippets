f7daf6ac08e727e85c911267a3922db0259d664c
Brian Brooks <brooks.brian@gmail.com>
2013-01-18 04:40:45 -0500

    4.3.cc

--- eopi/4.3.cc
+++ eopi/4.3.cc
@@ -15,6 +15,33 @@
 
 // Notice that if they merge, they must have the same tail node.
 
+template <typename T>
+int count_len(shared_ptr<node_t<T> > L) {
+  int len = 0;
+  while (L) {
+    ++len; L = L->next;
+  }
+  return len;
+}
+
+template <typename T>
+shared_ptr<node_t<T> > reachable_exists(
+    shared_ptr<node_t<T> > L1, shared_ptr<node_t<T> > L2) {
+  // Count the lengths of L1 and L2
+  int L1_len = count_len<T>(L1), L2_len = count_len<T>(L2);
+
+  while (L1_len > L2_len) {
+    L1 = L1->next; --L1_len;
+  }
+  while (L2_len > L1_len) {
+    L2 = L2->next; --L2_len;
+  }
+  while (L1 && L2 && L1 != L2) {
+    L1 = L1->next; L2 = L2->next;
+  }
+  return L1;
+}
+
 int main(int argc, char *argv[]) {
   
   return 0;

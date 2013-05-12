69dd3c15fc872c925914516aebb4e2615a11f690
Brian Brooks <brooks.brian@gmail.com>
2013-05-12 15:20:32 -0400

    ThreadSafeQueue.cc

---
+++ cia/ThreadSafeQueue.cc
@@ -0,0 +1,78 @@
+#include <queue>
+#include <mutex>
+#include <memory>
+#include <condition_variable>
+
+template<typename T>
+class threadsafe_queue {
+ private:
+  std::mutex mut;
+  std::queue<T> data_queue;
+  std::condition_variable data_cond;
+ public:
+  threadsafe_queue() {}
+  threadsafe_queue(threadsafe_queue const& other) {
+    std::lock_guard<std::mutex> lk(other.mut);
+    data_queue = other.data_queue;
+  }
+  void push(T new_value) {
+    std::lock_guard<std::mutex> lk(mut);
+    data_queue.push(new_value);
+    data_cond.notify_one();
+  }
+  void wait_and_pop(T& value) {
+    std::unique_lock<std::mutex> lk(mut);
+    data_cond.wait(lk, [this]{ return !data_queue.empty(); });
+    value = data_queue.front();
+    data_queue.pop();
+  }
+  std::shared_ptr<T> wait_and_pop() {
+    std::unique_lock<std::mutex> lk(mut);
+    data_cond.wait(lk, [this]{ return !data_queue.empty(); });
+    std::shared_ptr<T> res(std::make_shared<T>(data_queue.front()));
+    data_queue.pop();
+    return res;
+  }
+  bool try_pop(T& value) {
+    std::lock_guard<std::mutex> lk(mut);
+    if (data_queue.empty()) {
+      return false;
+    }
+    value = data_queue.front();
+    data_queue.pop();
+    return true;
+  }
+  std::shared_ptr<T> try_pop() {
+    std::lock_guard<std::mutex> lk(mut);
+    if (data_queue.empty()) {
+      return std::shared_ptr<T>();
+    }
+    std::shared_ptr<T> res(std::make_shared<T>(data_queue.front()));
+    data_queue.pop();
+    return res;
+  }
+  bool empty() const {
+    std::lock_guard<std::mutex> lk(mut);
+    return data_queue.empty();
+  }
+};
+
+threadsafe_queue<data_chunk> data_queue;
+
+void data_preparation_thread() {
+  while (more_data_to_prepare()) {
+    data_chunk const data = prepare_data();
+    data_queue.push(data);
+  }
+}
+
+void data_processing_thread() {
+  while (true) {
+    data_chunk data;
+    data_queue.wait_and_pop(data);
+    process(data);
+    if (is_last_chunk(data)) {
+      break;
+    }
+  }
+}

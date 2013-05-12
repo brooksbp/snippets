b263a231fcca9a9da9bc3041bb8efddae3819fd9
Brian Brooks <brooks.brian@gmail.com>
2013-05-12 11:26:12 -0400

    an escaping mutex

--- cia/BasicMutex.cc
+++ cia/BasicMutex.cc
@@ -15,3 +15,38 @@ bool list_contains(int value_to_find) {
                    some_list.end(),
                    value_to_find) != some_list.end();
 }
+
+
+// Structuring code to protect shared data
+
+class some_data {
+  int a;
+  std::string b;
+ public:
+  void do_something();
+};
+
+class data_wrapper {
+ private:
+  some_data data;
+  std::mutex m;
+ public:
+  template<typename Function>
+  void process_data(Function func) {
+    std::lock_guard<std::mutex> l(m);
+    func(data);
+  }
+};
+
+some_data *unprotected;
+
+void malicious_fn(some_data& protected_data) {
+  unprotected = &protected_data;
+}
+
+data_wrapper x;
+
+void foo() {
+  x.process_data(malicious_fn);
+  unprotected->do_something();
+}

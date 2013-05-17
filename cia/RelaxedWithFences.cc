cd31d8729a9eb3293f8140dd47c854f46ac54a26
Brian Brooks <brooks.brian@gmail.com>
2013-05-17 15:10:57 -0400

    RelaxedWithFences.cc

---
+++ cia/RelaxedWithFences.cc
@@ -0,0 +1,38 @@
+#include <atomic>
+#include <thread>
+#include <assert.h>
+
+//std::atomic<bool> x, y;
+bool x = false;
+std::atomic<bool> y;
+
+std::atomic<int> z;
+
+void write_x_then_y() {
+  //x.store(true, std::memory_order_relaxed);
+  x = true;
+  
+  std::atomic_thread_fence(std::memory_order_release);
+  y.store(true, std::memory_order_relaxed);
+}
+void read_y_then_x() {
+  while (!y.load(std::memory_order_relaxed)) { }
+  std::atomic_thread_fence(std::memory_order_acquire);
+  // if (x.load(std::memory_order_relaxed)) {
+  //   ++z;
+  // }
+  if (x) {
+    ++z;
+  }
+}
+int main(int argc, char *argv[]) {
+  x = false;
+  y = false;
+  z = 0;
+  std::thread a(write_x_then_y);
+  std::thread b(read_y_then_x);
+  a.join();
+  b.join();
+  assert(z.load() != 0);
+  return 0;
+}

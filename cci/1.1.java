9ce87a9ef4ca97041653030ec055dc8950f1c4c9
Brian Brooks <brooks.brian@gmail.com>
2013-05-30 21:19:18 -0400

    cleanup

--- cci/1.1.java
+++
@@ -1,8 +0,0 @@
-// 1.1 Implement an algorithm to determine if a string has all unique
-// characters.  What if you cannot use additional data structures?
-
-class cci_1_1 {
-  public static void main(String[] args) {
-    //System.out.println("Hello World!");
-  }
-}

d2fad91c12ad359a7793e223ec7110528618152e
Brian Brooks <brooks.brian@gmail.com>
2014-10-20 12:39:42 -0400

    mod

---
+++ c/mod.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("0 % 2 = %d\n", 0 % 2);
+  printf("1 % 2 = %d\n", 1 % 2);
+  printf("2 % 2 = %d\n", 2 % 2);
+  printf("3 % 2 = %d\n", 3 % 2);
+  printf("4 % 2 = %d\n", 4 % 2);
+  return 0;
+}
